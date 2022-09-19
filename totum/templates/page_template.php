f8aea7b13aa8d699ef1434efd94f93dc2b10cd3b
Tatiana <poluninat.m@gmail.com>
2022-09-19 16:43:03 +0300

    fx search errors

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=9c5ec9a"></script>
+    <script src="/js/main.js?v=3ca50db"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?25"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=39dbed3">
-    <script src="/js/main.js?v=9c5ec9a"></script>
+    <script src="/js/main.js?v=3ca50db"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?25"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
