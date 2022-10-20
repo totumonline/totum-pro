46b7c988e62810ea0df4f67b4a6a1ec40fe847c7
Tatiana <poluninat.m@gmail.com>
2022-10-20 17:05:13 +0300

    dark theme

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=50d05f1"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=95ff228">
+          href="/css/main.css?v=5b64409">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=6438448"></script>
+    <script src="/js/main.js?v=0171147"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?25"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
