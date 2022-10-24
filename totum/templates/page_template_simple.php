40d716e7cd70e50d8c81a242603e901f6af9d42f
Tatiana <poluninat.m@gmail.com>
2022-10-24 16:30:26 +0300

    dark theme for section codes

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=50d05f1"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=4cff134">
-    <script src="/js/main.js?v=9f90819"></script>
+          href="/css/main.css?v=3249327">
+    <script src="/js/main.js?v=0dff99e"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?25"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
