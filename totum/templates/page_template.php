38d0a5b485000463d4a95f1d745ebe0ad36b4e2c
Tatiana <poluninat.m@gmail.com>
2022-11-02 14:33:44 +0300

    fx dark scrolls

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=69fdade"></script>
+    <script src="/js/main.js?v=f5efa48"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?27"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
