196aa1dc23e3aae618f34520eb1cee8c60f2aa2a
Tatiana <poluninat.m@gmail.com>
2022-11-18 15:16:00 +0300

    fx dark-theme tree editor

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=50d05f1"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=3be65c0">
+          href="/css/main.css?v=eb943b7">
 
     <?php
     if ($isCreatorView ?? null) { ?>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=50d05f1"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=3be65c0">
+          href="/css/main.css?v=eb943b7">
     <script src="/js/main.js?v=0ea78dd"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?28"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
