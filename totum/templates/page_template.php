ee3ef47798461f6e8a1b7d87a3aaf88b1ea7faf0
Tatiana <poluninat.m@gmail.com>
2022-11-22 16:40:22 +0300

    dark theme for mobile

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=d88227e"></script>
+    <script src="/js/main.js?v=8fe02c3"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?28"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=38b659b">
-    <script src="/js/main.js?v=d88227e"></script>
+    <script src="/js/main.js?v=8fe02c3"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?28"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
