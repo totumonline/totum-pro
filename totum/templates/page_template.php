ccce9a220b1604b3be47605c8f8af38e00e798c3
Tatiana <poluninat.m@gmail.com>
2022-11-24 14:48:09 +0300

    dark theme improvements

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=50d05f1"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=dabd496">
+          href="/css/main.css?v=0a1a0a6">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=b87aaa7"></script>
+    <script src="/js/main.js?v=17498d8"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?28"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
