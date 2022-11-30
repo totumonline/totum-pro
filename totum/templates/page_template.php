cd1fb071e10c71d42c952f566c7b9671181303fb
Tatiana <poluninat.m@gmail.com>
2022-11-30 17:22:44 +0300

    quick preview for secure files

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=91ea8c3"></script>
+    <script src="/js/main.js?v=aac884d"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=c106e7c">
-    <script src="/js/main.js?v=91ea8c3"></script>
+    <script src="/js/main.js?v=aac884d"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
