262946ce468060f150660a6b4ef10b460627d601
Tatiana <poluninat.m@gmail.com>
2022-12-05 18:43:03 +0300

    Merge remote-tracking branch 'upstream/dev' into dev

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -11,14 +11,14 @@
 
     <?php
     if ($isCreatorView ?? null) { ?>
-        <script src="/js/functions.js?v=eafcaa5"></script>
+        <script src="/js/functions.js?v=e6e80c7"></script>
         <?php
            echo '<script>App.functions=App.functions.concat(' . $this->Config->getExtFunctionsTemplates() . ')</script>';
         ?>
         <?php
     } ?>
 
-    <script src="/js/main.js?v=085a735"></script>
+    <script src="/js/main.js?v=2b070e9"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
