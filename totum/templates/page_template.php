480efe769b3b622596e1a7becf89cd798e7cf773
Tatiana <poluninat.m@gmail.com>
2022-12-30 18:10:37 +0300

    Merge remote-tracking branch 'upstream/dev' into dev

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,8 +18,8 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=e303eb2"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
+    <script src="/js/main.js?v=11717da"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?4"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=1dd26f4">
-    <script src="/js/main.js?v=e303eb2"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
+    <script src="/js/main.js?v=11717da"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?4"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
