4e4b5d58e2e6f5fc52b80610981b2f3cac4a3a34
Tatiana <poluninat.m@gmail.com>
2022-12-30 17:30:49 +0300

    PRO admin highlighting

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=50d05f1"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=307c67d">
-    <script src="/js/main.js?v=886aba1"></script>
+          href="/css/main.css?v=1dd26f4">
+    <script src="/js/main.js?v=e303eb2"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
