712e8e293ea05c1032be13808ed0c809a89fa1e5
Tatiana <poluninat.m@gmail.com>
2023-04-12 19:14:53 +0300

    dark theme improvements

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=50d05f1"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=a385ca7">
-    <script src="/js/main.js?v=067506d"></script>
+          href="/css/main.css?v=5e101f2">
+    <script src="/js/main.js?v=5b8462a"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?8"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
