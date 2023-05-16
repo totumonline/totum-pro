c3ac5729e860b0516bad631f35eb4a431376d700
Tatiana <poluninat.m@gmail.com>
2023-05-16 18:29:18 +0300

    file versions improves && fixes

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=50d05f1"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=ede9e88">
-    <script src="/js/main.js?v=3f6fb7c"></script>
+          href="/css/main.css?v=65a511d">
+    <script src="/js/main.js?v=127fafe"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
