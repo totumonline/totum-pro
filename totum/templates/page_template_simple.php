badff1259a4273481c89111514a9ef9ef7ae5a49
Tatiana <-->
2023-05-30 13:12:58 +0300

    viewtypes for editPanels

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=50d05f1"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=1bc55f9">
-    <script src="/js/main.js?v=7ddaf27"></script>
+          href="/css/main.css?v=14a422e">
+    <script src="/js/main.js?v=61e3f0f"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
