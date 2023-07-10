003c89741364f24207e57f72037a14675b5e19b3
Tatiana <-->
2023-07-10 16:06:59 +0300

    versions improve

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=63416c5"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=bfc61b2">
+          href="/css/main.css?v=0412262">
 
     <?php
     if ($isCreatorView ?? null) { ?>
