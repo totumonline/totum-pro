f6f9fb919b5a50dab6dc895537fdbb3900e3f2a9
Tatiana <-->
2023-08-15 12:05:06 +0300

    OnlyOffice rm new-docs without onlyoffice

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=d333433"></script>
+    <script src="/js/main.js?v=a070acd"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=f670171">
-    <script src="/js/main.js?v=d333433"></script>
+    <script src="/js/main.js?v=a070acd"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
