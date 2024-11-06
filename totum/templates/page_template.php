fd64679daba12bf65b068ca84cd33eb801a8544f
Tatiana <-->
2024-11-06 16:00:06 +0300

    aiSettings

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=bcf924b"></script>
+    <script src="/js/main.js?v=6d6be63"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
