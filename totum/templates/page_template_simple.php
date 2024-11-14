0b6ea72c394de4360ffb0d1d3625859260f155dd
Tatiana <-->
2024-11-14 12:43:25 +0300

    ai focus

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=b396510">
-    <script src="/js/main.js?v=1dfbb6d"></script>
+    <script src="/js/main.js?v=e43e452"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
