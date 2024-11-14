6d4bbe261f3542649e41b07fd328d04237206b24
Tatiana <-->
2024-11-14 12:51:02 +0300

    ai ask_selected

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=b396510">
-    <script src="/js/main.js?v=e43e452"></script>
+    <script src="/js/main.js?v=2b4b98c"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
