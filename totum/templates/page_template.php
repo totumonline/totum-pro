093168500e49aabf08ff3f6b23e40476bcb0c959
Tatiana <-->
2024-11-20 14:11:56 +0300

    ai upds

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=b101ece"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=4af88d0">
+          href="/css/main.css?v=e0f2b38">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=34c6275"></script>
+    <script src="/js/main.js?v=b74bd58"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
