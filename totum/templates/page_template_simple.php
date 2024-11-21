706bae913c704a9cb78df45f36c61ae8acf7175b
Tatiana <-->
2024-11-21 11:43:24 +0300

    ai css up

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=b101ece"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=58e1b0a">
+          href="/css/main.css?v=39fe7bb">
     <script src="/js/main.js?v=565113f"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
