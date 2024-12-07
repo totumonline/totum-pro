eba6aa1edc0f6e217f34669ee2b623e9b707628e
Tatiana <-->
2024-12-07 13:57:01 +0300

    fx notifications

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=f66313f">
-    <script src="/js/main.js?v=7e44cea"></script>
+    <script src="/js/main.js?v=b116751"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
