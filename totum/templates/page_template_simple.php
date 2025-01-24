01c2f0f3133a2e4ddb6b5c3e35ecb7d57c63e20e
Tatiana <-->
2025-01-24 17:36:22 +0300

    notifications && ai front fxs

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=b101ece"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=6dc8fa6">
-    <script src="/js/main.js?v=7e87fc5"></script>
+          href="/css/main.css?v=cea2bcc">
+    <script src="/js/main.js?v=af9cfb7"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?12"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
