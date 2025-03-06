6f7fe648035d257bbbcf676ec7ba3f5d196872b0
Tatiana <-->
2025-03-06 08:58:28 +0300

    Merge remote-tracking branch 'upstream/dev' into dev

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=cdb421c">
+          href="/css/main.css?v=4d1bf1b">
 
     <?php
     if ($isCreatorView ?? null) { ?>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=cdb421c">
+          href="/css/main.css?v=4d1bf1b">
     <script src="/js/main.js?v=dfc2623"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
