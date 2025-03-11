85f680d84ba4df0821a43e5ff5cb9fee3f38386b
Tatiana <-->
2025-03-11 16:54:20 +0300

    fx spec funcs

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=1f9d2f8"></script>
+    <script src="/js/main.js?v=fa16f9a"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
