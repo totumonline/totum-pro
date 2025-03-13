d5c9bc6b595e81786768b6dbd83fc233348b07cf
Tatiana <-->
2025-03-13 11:25:36 +0300

    fx spec funcs

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=6e93991">
+          href="/css/main.css?v=6300c6f">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=2826a39"></script>
+    <script src="/js/main.js?v=956bdc4"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
