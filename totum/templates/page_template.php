300c758ca965a42c21b9b13577f8ea0f89c6a5e4
Tatiana <-->
2025-03-25 09:13:22 +0300

    fx selected cells

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=57af382"></script>
+    <script src="/js/main.js?v=943a88f"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=bbad8a0">
-    <script src="/js/main.js?v=57af382"></script>
+    <script src="/js/main.js?v=943a88f"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
