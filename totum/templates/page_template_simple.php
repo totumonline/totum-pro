bac6d3a9b744ff4812d20587a6227bb7d17523fd
Tatiana <-->
2025-06-01 16:40:57 +0300

    panelsView panel scroll titles and  icon css fx

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=cf6dcbb">
-    <script src="/js/main.js?v=6bbd3f8"></script>
+          href="/css/main.css?v=f1bc204">
+    <script src="/js/main.js?v=69a928b"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
