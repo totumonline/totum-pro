252e217d93ab744f1c0f70df4ac4c811c4bee2ea
Tatiana <-->
2025-06-11 08:48:25 +0300

    fx treeView

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=307d5fc"></script>
+    <script src="/js/main.js?v=12e3108"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
