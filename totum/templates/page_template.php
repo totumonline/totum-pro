23c29b9d1140cd0212a3e8228af3b329d81e9a9f
Tatiana <-->
2025-08-02 19:09:34 +0300

    specFuncs improves

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=6d3c34d"></script>
+    <script src="/js/main.js?v=8217fea"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=ebf96f8">
-    <script src="/js/main.js?v=6d3c34d"></script>
+    <script src="/js/main.js?v=8217fea"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
