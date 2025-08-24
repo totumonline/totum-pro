43a6dea9c1102070fe02e7355deb7e49ddf6645f
Tatiana <-->
2025-08-24 08:49:48 +0300

    dark theme css

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=c961752">
+          href="/css/main.css?v=65090db">
 
     <?php
     if ($isCreatorView ?? null) { ?>
