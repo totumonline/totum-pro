1032f6f3b7e3b4a7aa0b057773e6010fa15eb4c9
Tatiana <-->
2025-09-04 16:55:06 +0300

    buttons css upd

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=7d42c33">
+          href="/css/main.css?v=84d4da4">
     <script src="/js/main.js?v=cff171b"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
