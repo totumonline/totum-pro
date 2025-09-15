dedbcc5fd73d78b006895647b3f9977afa5d66a6
Tatiana <-->
2025-09-15 12:43:37 +0300

    gomodule daemons && dark theme css

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=eabfa2e">
+          href="/css/main.css?v=894b854">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=cff171b"></script>
+    <script src="/js/main.js?v=dc16c95"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=eabfa2e">
-    <script src="/js/main.js?v=cff171b"></script>
+          href="/css/main.css?v=894b854">
+    <script src="/js/main.js?v=dc16c95"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
