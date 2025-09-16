11eaa44d2cc56e421b36a984b8d9bed291e6e921
Tatiana <-->
2025-09-16 14:09:54 +0300

    dark theme css upds

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=894b854">
+          href="/css/main.css?v=1c0ae76">
     <script src="/js/main.js?v=dc16c95"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
