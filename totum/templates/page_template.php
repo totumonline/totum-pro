5c4d21c191ba50ce8f716097032d5b554061f52b
Tatiana <-->
2025-09-22 15:35:53 +0300

    fx edit

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=380810e"></script>
+    <script src="/js/main.js?v=aba3310"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
