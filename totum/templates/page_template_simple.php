fd8a6b3a50ed86ada6349d785543abc0cce66e67
Tatiana <-->
2025-11-15 15:45:18 +0300

    fx floats

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=0c249b5">
-    <script src="/js/main.js?v=2194ff4"></script>
+          href="/css/main.css?v=4e28b75">
+    <script src="/js/main.js?v=8787f33"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?14"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
