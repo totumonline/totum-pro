d08ba24c8420b8a436939fff1d1d301373e09748
Tatiana <-->
2026-03-30 18:00:52 +0300

    fx buttonClick

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=055fcb4">
+          href="/css/main.css?v=5272adc">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=d0183b0"></script>
+    <script src="/js/main.js?v=d08eeea"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?14"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
