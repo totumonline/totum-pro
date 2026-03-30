1665d482b17188a9b60181b2b7898e15d2189ed0
Tatiana <-->
2026-03-30 19:49:17 +0300

    fx buttonClick

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=5272adc">
-    <script src="/js/main.js?v=d08eeea"></script>
+    <script src="/js/main.js?v=4b9c3dc"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?14"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
