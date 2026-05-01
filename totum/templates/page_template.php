f402f7eae7c597677cfae3e03095abfa6286aea1
Tatiana <-->
2026-05-01 16:40:21 +0300

    navButtonTree

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=63c5d0e">
+          href="/css/main.css?v=dc1857e">
 
     <?php
     if ($isCreatorView ?? null) { ?>
