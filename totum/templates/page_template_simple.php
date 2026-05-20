849911acfa362e66fe6f1faf722ca7684562ec50
Tatiana <-->
2026-05-20 14:55:48 +0300

    rm dropdowns in mobile nav menu

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=dc1857e">
+          href="/css/main.css?v=d012326">
     <script src="/js/main.js?v=325d440"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?14"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
