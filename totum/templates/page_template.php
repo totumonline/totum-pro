f58fcc696f1fa035a97745a13155d5622ab197ce
Tatiana <-->
2026-06-01 19:28:18 +0300

    fx button select

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=9828831"></script>
+    <script src="/js/main.js?v=dc1bfdd"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?14"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
